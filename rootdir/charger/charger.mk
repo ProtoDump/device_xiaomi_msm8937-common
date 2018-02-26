c93d059288a78a137d1079633f367862b742365b
Shubham Das <dasshubham762@gmail.com>
2018-02-26 18:37:54 +0530

    msm8937-common: rootdir: Import xiaomi charger
    Surprisingly it is working very well while offline charging, same as MIUI
    
    Reference- https://review.lineageos.org/#/c/205385/
    
    Change-Id: I1282a368bc9b0b9cd51cbbcde8d9973dce822de6

---
+++ rootdir/charger/charger.mk
@@ -0,0 +1,21 @@
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/charger/chargeonlymode:$(TARGET_OUT_DIR)/root/sbin/charger \
+    $(LOCAL_PATH)/rootdir/charger/battery_image_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_0.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_image_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_1.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_image_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_2.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_image_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_3.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_image_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_4.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_image_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_5.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_image_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_6.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_image_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_7.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_num_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_0.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_num_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_1.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_num_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_2.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_num_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_3.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_num_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_4.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_num_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_5.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_num_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_6.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_num_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_7.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_num_8.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_8.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_num_9.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_9.png \
+    $(LOCAL_PATH)/rootdir/charger/battery_num_10.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_10.png
