fd6d47cc0ac99ac08c8e9d8c0d1d8e8c102993e2
Akmal <magchuz@pm.me>
2020-09-10 06:27:50 +0000

    msm8937-common: Use VNDK Version current
    * Also shim libandroid.so for camera

--- msm8937.mk
+++ msm8937.mk
@@ -334,6 +334,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+# Shims
+PRODUCT_PACKAGES += \
+    libcamshim
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common_system \
