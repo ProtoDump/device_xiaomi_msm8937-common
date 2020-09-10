466a5831e096ed1ddb08d8287e469bafaa562ab6
Akmal <magchuz@pm.me>
2020-09-10 10:12:21 +0000

    msm8937-common: Remove VNDK-SP

--- msm8937.mk
+++ msm8937.mk
@@ -371,10 +371,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# VNDK-SP
-PRODUCT_PACKAGES += \
-    vndk-sp
-
 # Vold
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
