9cb50f4498f500319053c838f7a96cc651b0a042
GiaSen <giammasena@gmail.com>
2020-09-20 08:19:26 +0000

    msm8937-common: Build libhwbinder

--- msm8937.mk
+++ msm8937.mk
@@ -27,6 +27,11 @@ DEVICE_PACKAGE_OVERLAYS += \
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
