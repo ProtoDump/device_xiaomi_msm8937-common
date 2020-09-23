043e365093beae738b99bfc424b21e5ac664faba
Akmal <magchuz@pm.me>
2020-09-23 22:50:32 +0000

    msm8937-common: Use sepolicy_vndr for sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -208,7 +208,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_RECOVERY_FSTAB 		 := $(VENDOR_PATH)/rootdir/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Treble
